7d517b7bd32eea3f01053a180b3dfdeedb553c07
jarbasal <jarbasai@mailfence.com>
2020-06-09 20:32:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# <img src='./icon.png' width='50' height='50' style='vertical-align:bottom'/> Confucius Quotes
+
+
+## About
+
+Astronomy Picture of the Day
+  
+![](gui.png)
+
+## Examples
+* "Astronomy picture of the day"
+* "explain"
+
+## Credits
+JarbasAl
+
+## Category
+**Entertainment**
+
+## Tags
+#nasa
---
+++ __init__.py
@@ -0,0 +1,57 @@
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
+import requests
+from mycroft.skills.core import resting_screen_handler
+from adapt.intent import IntentBuilder
+import time
+
+
+class AstronomyPictureOfThedaySkill(MycroftSkill):
+    def __init__(self):
+        super(AstronomyPictureOfThedaySkill, self).__init__(
+            name="AstronomyPictureOfTheday")
+        if "nasa_key" not in self.settings:
+            self.settings["nasa_key"] = "DEMO_KEY"
+
+    def update_picture(self):
+        try:
+            # TODO check date and save calls
+
+            apod_url = "https://api.nasa.gov/planetary/apod?api_key=" + self.settings["nasa_key"]
+            response = requests.get(apod_url).json()
+            title = response["title"]
+            url = response["url"]
+            summary = response["explanation"]
+
+            self.settings['imgLink'] = url
+            self.settings['title'] = title
+            self.settings['summary'] = summary
+            self.settings["ts"] = time.time()
+        except:
+            pass
+        self.gui['imgLink'] = self.settings['imgLink']
+        self.gui['title'] = self.settings['title']
+        self.gui['summary'] = self.settings['summary']
+
+    @resting_screen_handler("APOD")
+    def idle(self, message):
+        self.update_picture()
+        self.gui.show_page('idle.qml')
+
+    @intent_file_handler('apod.intent')
+    def handle_apod(self, message):
+        self.update_picture()
+
+        self.gui.show_image(self.settings['imgLink'],
+                            caption= self.settings['title'],
+                            fill='PreserveAspectFit')
+
+        self.speak(self.settings['title'])
+        self.set_context("APOD")
+
+    @intent_handler(IntentBuilder("ExplainIntent")
+                    .require("ExplainKeyword").require("APOD"))
+    def handle_explain_why(self, message):
+        self.speak( self.settings['summary'])
+
+def create_skill():
+    return AstronomyPictureOfThedaySkill()
