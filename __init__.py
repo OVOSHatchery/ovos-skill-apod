d42dfcd0e99f8c0fdfea4fa3af2d4fba2533c740
jarbasal <jarbasai@mailfence.com>
2020-06-25 22:56:23 +0100

    requests caching

--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
 from mycroft import MycroftSkill, intent_file_handler, intent_handler
-import requests
 from mycroft.skills.core import resting_screen_handler
 from adapt.intent import IntentBuilder
 from mtranslate import translate
-from datetime import datetime, timedelta
+from requests_cache import CachedSession
+from datetime import timedelta, datetime
+
 
 
 class AstronomyPictureOfThedaySkill(MycroftSkill):
@@ -12,27 +13,24 @@ class AstronomyPictureOfThedaySkill(MycroftSkill):
             name="AstronomyPictureOfTheday")
         if "nasa_key" not in self.settings:
             self.settings["nasa_key"] = "DEMO_KEY"
+        _expire_after = timedelta(hours=1)
+        self._session = CachedSession(backend='memory',
+                                      expire_after=_expire_after)
 
     def update_picture(self):
         try:
-            today = datetime.now().replace(hour=12, second=0, minute=0,
-                                           microsecond=0)
-            if not self.settings.get("ts"):
-                self.settings["ts"] = (today - timedelta(days=1)).timestamp()
-            if today.timestamp() != self.settings["ts"] or not self.settings.get('imgLink'):
-                apod_url = "https://api.nasa.gov/planetary/apod?api_key=" + self.settings["nasa_key"]
-                response = requests.get(apod_url).json()
-                title = response["title"]
-                url = response["url"]
-                summary = response["explanation"]
-                if not self.lang.lower().startswith("en"):
-                    summary = translate(summary, self.lang)
-                    title = translate(title, self.lang)
+            apod_url = "https://api.nasa.gov/planetary/apod?api_key=" + self.settings["nasa_key"]
+            response = self._session.get(apod_url).json()
+            title = response["title"]
+            url = response["url"]
+            summary = response["explanation"]
+            if not self.lang.lower().startswith("en"):
+                summary = translate(summary, self.lang)
+                title = translate(title, self.lang)
 
-                self.settings['imgLink'] = url
-                self.settings['title'] = title
-                self.settings['summary'] = summary
-                self.settings["ts"] = today.timestamp()
+            self.settings['imgLink'] = url
+            self.settings['title'] = title
+            self.settings['summary'] = summary
         except Exception as e:
             self.log.exception(e)
         self.gui['imgLink'] = self.settings['imgLink']
